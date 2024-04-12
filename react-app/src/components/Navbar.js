37ca206c93c66cf9b3bc7a9dcc36a98fd0e54740
Jerry Du <jduomaha@gmail.com>
2024-04-12 05:00:00 -0400

    Made initial landing page for website

---
+++ react-app/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, AppBar, Toolbar, Typography, ThemeProvider } from '@mui/material';
+import theme from '.././hooks/useTheme.js'
+
+function Navbar() {
+  return (
+    <div>
+    <ThemeProvider theme={theme}>
+    <AppBar position="static" color = "primary">
+      <Toolbar>
+        <Typography variant="h6">Rattigan's Reviews</Typography>
+        <Button color="inherit" style={{ marginLeft: '40px' }}>TV Shows </Button>
+        <Button color="inherit" style={{ marginLeft: '40px' }}>Search </Button>
+        <Button color="inherit" style={{ marginLeft: '40px' }}>Movies </Button>
+      </Toolbar>
+    </AppBar>
+    </ThemeProvider>
+  </div>
+  );
+}
+
+export default Navbar;
