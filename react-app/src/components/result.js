8798f7179eaf9d08b902dfed1e141fa43179a8fe
Omar Natour <omarjnatour@gmail.com>
2024-04-17 15:45:36 -0400

    scaffolding for results component

---
+++ react-app/src/components/result.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, AppBar, Toolbar, Typography, ThemeProvider } from '@mui/material';
+import theme from '../hooks/useTheme.js'
+import style from '../styles/Result.module.css';
+
+function Result(){
+    return(
+
+        <div className = {style.container}>
+            <div className = {style.coverArt}>
+
+                <p> pretty picture :)</p>
+
+            </div>
+
+            <div className = {style.info}>
+
+                <p> lots of info</p>
+
+            </div>
+            
+            
+        </div>
+
+    )
+
+}
+
+export default Result
