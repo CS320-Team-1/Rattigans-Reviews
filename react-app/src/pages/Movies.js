cd0d85e3f1a385b6f1318aca899b618a1618f41c
JonCoombs <jdcoombs@umass.edu>
2024-04-28 18:12:01 -0400

    Added TV Shows, Movies, and Anime

--- react-app/src/pages/Movies.js
+++ react-app/src/pages/Movies.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Navbar from '../components/Navbar.js'
+import Searchbar from '../components/Searchbar.js'
+import Trending from '../components/Trending.js'
+
 function Movies() {
 
     return (
         <div>
-            <Navbar/>
+            <Navbar>
+            </Navbar>
+            <Searchbar>
+            </Searchbar>
+            <Trending>
+            </Trending>
         </div>
     )
 }
