8dfc4c1fbc5854dce780bbc49e1325b29cee284c
Omar Natour <omarjnatour@gmail.com>
2024-05-01 15:40:27 -0400

    imported mui carousel

--- react-app/src/pages/TVShows.js
+++ react-app/src/pages/TVShows.js
@@ -3,6 +3,9 @@ import Navbar from '../components/Navbar.js'
 import Searchbar from '../components/Searchbar.js'
 import Trending from '../components/Trending.js'
 
+import Carousel from '../components/Carousel.js'
+
+
 function TVShows() {
     
     return(
@@ -13,6 +16,8 @@ function TVShows() {
             </Searchbar>
             <Trending>
             </Trending>
+            <Carousel/>
+            
         </div>
     )
 }
